606a8d9e963708d5ab1caa57d001a481570246b4
Aagnes Precella <agnesarmy2005@gmail.com>
2026-02-26 10:44:22 +0530

    Remove duplicate app folder from root directory

--- app/models/student_model.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from app.core.db import Base
-
-class Student(Base):
-    __tablename__ = "students"
-    
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False)
-    email = Column(String, unique=True, nullable=False, index=True)
